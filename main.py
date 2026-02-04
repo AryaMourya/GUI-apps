e130ea1b3de148eec9557717e8e7f769b2451cde
AryaMourya <aryamourya0@gmail.com>
2026-02-04 12:27:35 +0530

    Implement file open and save functionality in the text editor

--- main.py
+++ main.py
@@ -16,4 +16,36 @@ text_area.pack(expand=True, fill=tk.BOTH)
 def new_file():
     text_area.delete(1.0, tk.END)
 
+def open_file():
+    #open file dialogue
+    file_path = filedialog.askopenfilename(
+        defaultextension=".txt",
+        filetypes=[("Text files","*.txt")]
+    )
+    if file_path:
+        #open file
+        with open(file_path,"r") as file:
+            text_area.delete(1.0, tk.END)
+            text_area.insert(tk.END, file.read())
+
+def save_file():
+    #save file 
+    file_path =filedialog.asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text files","*.txt")]
+    )
+    if file_path:
+        with open(file_path,"w") as file:
+            file.write(text_area.get(1.0, tk.END))
+
+    messagebox.showinfo("info","File saved successfully!")
+
+##MENU##
+menu_bar =tk.Menu(main)
+main.config(menu=menu_bar)
+
+file_menu = tk.Menu(menu_bar)
+#new file,open file,save file,exit
+
+
 main.mainloop()
